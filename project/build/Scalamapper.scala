9cd9619e22cb261b1d31282d66a18c13e8a7e37e
maiha <maiha@wota.jp>
2010-05-15 17:49:23 +0900

    I'd like to run tests

---
+++ project/build/Scalamapper.scala
@@ -0,0 +1,5 @@
+import sbt._
+
+class ScalamapperProject(info: ProjectInfo) extends DefaultProject(info) {
+  val scalamapperVersion = "0.1"
+}
---
+++ src/main/scala/jp/wota/scalamapper/Resource.scala
@@ -0,0 +1,4 @@
+package jp.wota.scalamapper
+
+trait Resource {
+}
