5e8d12ad548a9fb7279879b60fedb23fa47b5afb
maiha <maiha@wota.jp>
2010-05-16 14:02:29 +0900

    refactor storage.Repository

--- src/main/scala/jp/wota/scalamapper/storage/Repository.scala
+++ src/main/scala/jp/wota/scalamapper/storage/Repository.scala
@@ -13,29 +13,18 @@ case class Repository(connection:Connection, keyspace: String, standard: String)
   val consistency   = ConsistencyLevels.quorum
   val client        = Client(connection, keyspace, serialization, consistency)
 
-//  case class Row(key:String)
-
-  case class StandardColumn(name:String) {
-    val parent = client.ColumnParent(standard, None)
-    val path   = client.ColumnPath(standard, None, name)
+  class AbstractColumn(name:String, group:Option[String]) {
+    val parent = client.ColumnParent(standard, group)
+    val path   = client.ColumnPath(standard, group, name)
 
     def get(key:String): Option[String] = client.get(key, path)
     def set(key:String, value:String)   = client.update(key, path, value.toString)
     def del(key:String)                 = client.remove(key, path)
     def count(key:String)               = client.count(key, parent)
-//    def exist(key:String): Nothing
   }
-  
-  case class SuperColumn(name:String, row:String) {
-    val parent = client.ColumnParent(standard, Some(row))
-    val path   = client.ColumnPath(standard, Some(row), name)
 
-    def get(key:String): Option[String] = client.get(key, path)
-    def set(key:String, value:String)   = client.update(key, path, value.toString)
-    def del(key:String)                 = client.remove(key, path)
-    def count(key:String)               = client.count(key, parent)
-//    def exist(key:String): Nothing
-  }
+  case class StandardColumn(name:String)            extends AbstractColumn(name, None)
+  case class SuperColumn(name:String, group:String) extends AbstractColumn(name, Some(group))
 }
 
 
